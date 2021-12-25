0708d53cac346dade930cb47ba626aadbb05d6bc
Guilherme Ferraz <guilherme.ferraz@portergroup.com.br>
2021-12-25 19:23:59 -0300

    feat: assemble login screen

---
+++ src/constants/currencies.ts
@@ -0,0 +1,6 @@
+export const currencyOptions = [
+  { label: 'US Dollar', value: '$', key: 'usd' },
+  { label: 'Brazilian Real', value: 'R$', key: 'brl' },
+  { label: 'Canadian Dolar', value: 'CA$', key: 'cnd' },
+  { label: 'Euro', value: '€', key: 'euro' },
+];
