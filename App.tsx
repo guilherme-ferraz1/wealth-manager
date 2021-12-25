5019cd81d062099d5d58865ae59a719d5dff29f8
Guilherme Ferraz <guilherme.ferraz@portergroup.com.br>
2021-12-25 19:21:39 -0300

    feat: setup auth context

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable global-require */
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './src/navigators';
+import { theme } from './src/styles';
+import AuthContext from './src/contexts/auth';
 
 export default function App(): JSX.Element {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const value = { loggedIn, setLoggedIn };
   const [fontsLoaded] = useFonts({
     'Avenir-Next-Regular': require('./src/assets/fonts/AvenirNextLTPro-Regular.ttf'),
     'Avenir-Next-Bold': require('./src/assets/fonts/AvenirNextLTPro-Bold.ttf'),
@@ -16,10 +20,18 @@ export default function App(): JSX.Element {
     return <AppLoading />;
   }
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView
+        style={{ flex: 0, backgroundColor: theme.colors.primary }}
+      />
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <AuthContext.Provider value={value}>
+            <RootNavigator />
+          </AuthContext.Provider>
+        </NavigationContainer>
+      </SafeAreaView>
+    </>
   );
 }
--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Button } from 'react-native';
 import Text from '../../components/atoms/Text';
+import AuthContext from '../../contexts/auth';
+import { removeData } from '../../services';
 
 const HomeScreen: React.FC = (): JSX.Element => {
+  const { setLoggedIn } = useContext(AuthContext);
+  const logout = async () => {
+    await removeData({ key: 'userName' });
+    await removeData({ key: 'userCurrency' });
+    setLoggedIn(false);
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text> Home </Text>
+      <Button title="logout" onPress={() => logout()} />
     </View>
   );
 };
